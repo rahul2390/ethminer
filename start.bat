c476a11580010830cfd11bdfc5bdf2ec87844f65
rahul2390 <58142479+rahul2390@users.noreply.github.com>
2019-11-24 21:40:47 +0530

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+EthDcrMiner64.exe -epool eth-asia1.nanopool.org:9999 -ewal 0xCd4B3edB0A4B4D2d30481A7dFc4b56b7c31a89d3 -epsw x -rxboost 1
